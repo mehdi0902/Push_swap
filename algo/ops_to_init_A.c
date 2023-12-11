ab7fea40a4f7da0c15c1499e93d16ea4e8799537
Mehdi Ben Abdelhadi <mben-abd@c1r16s3.42lausanne.ch>
2023-12-11 13:41:46 +0100

    go

--- algo/ops_to_init_A.c
+++ algo/ops_to_init_A.c
@@ -1,112 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ops_to_init_A.c                                    :+:      :+:    :+:   */
+/*   ops_to_init_B.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mben-abd <mben-abd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/27 16:58:46 by mben-abd          #+#    #+#             */
-/*   Updated: 2023/12/11 11:01:36 by mben-abd         ###   ########.fr       */
+/*   Created: 2023/12/01 22:03:19 by mben-abd          #+#    #+#             */
+/*   Updated: 2023/12/05 20:17:55 by mben-abd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap1.h"
 
-void	mid_index(t_stack_n *stack)
+void	aim_for_a(t_stack_n *a, t_stack_n *b)
 {
-	int	i;
-	int	mid;
-
-	i = 0;
-	if (!stack)
-		return ;
-	mid = ft_stack_size(stack) / 2;
-	while (stack)
-	{
-		stack->index = i;
-		if (i <= mid)
-			stack->middle_up = true;
-		else
-			stack->middle_up = false;
-		stack = stack->next;
-		++i;
-	}
-}
-
-void	aim_for_b(t_stack_n *a, t_stack_n *b)
-{
-	t_stack_n	*pointeur_b;
+	t_stack_n	*pointeur_a;
 	t_stack_n	*node_target;
 	long		best_comp;
 
-	while (a)
+	while (b)
 	{
 		best_comp = LONG_MIN;
-		pointeur_b = b;
-		while (pointeur_b)
+		pointeur_a = a;
+		while (pointeur_a)
 		{
-			if (pointeur_b->nbr > a->nbr && pointeur_b->nbr > best_comp)
+			if (pointeur_a->nbr > a->nbr && pointeur_a->nbr > best_comp)
 			{
-				best_comp = pointeur_b->nbr;
-				node_target = pointeur_b;
+				best_comp = pointeur_a->nbr;
+				node_target = pointeur_a;
 			}
-			pointeur_b = pointeur_b->next;
-			if (best_comp == LONG_MIN)
-				a->target_n = find_max(b);
+			pointeur_a = pointeur_a->next;
+			if (best_comp == LONG_MAX)
+				b->target_n = find_max(b);
 			else
-				a->target_n = node_target;
-			a = a->next;
+				b->target_n = node_target;
+			b = b->next;
 		}
 	}
 }
 
-void	cost_a(t_stack_n *a, t_stack_n *b)
-{
-	int	size_a;
-	int	size_b;
-
-	size_a = ft_stack_size(a);
-	size_b = ft_stack_size(b);
-	while (a)
-	{
-		a->cmd_cost = a->index;
-		if (!(a->middle_up))
-			a->cmd_cost = size_a - (a->index);
-		if (a->target_n->middle_up)
-			a->cmd_cost += a->target_n->index;
-		else
-			a->cmd_cost += size_b - (a->target_n->index);
-		a = a->next;
-	}
-}
-
-void	set_cheap(t_stack_n *stack)
-{
-	long		cheap_value;
-	t_stack_n	*cheapest_n;
-
-	if (!stack)
-		return ;
-	// assigne en long pour rentrer obligatoirement dans le if
-	cheap_value = LONG_MAX;
-	while (stack)
-	{
-		if (stack->cmd_cost < cheap_value)
-		{
-			// rentre dans le if et met a jour le noeud jusqu'a qu'il soit egale a cmd_cost
-			cheap_value = stack->cmd_cost;
-			cheapest_n = stack;
-		}
-		stack = stack->next;
-	}
-	cheapest_n->cheap = true;
-}
-
-void	init_a_to_b(t_stack_n *a, t_stack_n *b)
+void	init_b_to_a(t_stack_n *a, t_stack_n *b)
 {
 	mid_index(a);
 	mid_index(b);
 	aim_for_a(a, b);
-	cost_a(a, b);
-	set_cheap(a);
 }
