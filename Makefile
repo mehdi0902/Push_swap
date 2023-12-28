98050b66448502a3afeb0383394ba779ce3df28c
Mehdi Ben Abdelhadi <mben-abd@c1r17s1.42lausanne.ch>
2023-12-28 17:14:39 +0100

    100/125 pour listant

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC = gcc
-CFLAGS = -Wall -Wextra -std=c99
+CFLAGS = -Wall -Wextra -Werror
 LIBS = libft2/libft.a
 
 SRC_DIR_ALGO = algo
--- algo/main.c
+++ algo/main.c
@@ -6,7 +6,7 @@
 /*   By: mben-abd <mben-abd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 12:03:43 by mben-abd          #+#    #+#             */
-/*   Updated: 2023/12/26 23:59:25 by mben-abd         ###   ########.fr       */
+/*   Updated: 2023/12/28 17:11:17 by mben-abd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,25 @@
 
 int	main(int ac, char **av)
 {
-	t_stack_n	**a;
-	t_stack_n	**b;
+	t_stack_n	*a;
+	t_stack_n	*b;
 
 	a = NULL;
 	b = NULL;
-	a = malloc(sizeof(t_stack_n) * ac - 1);
-	b = malloc(sizeof(t_stack_n) * ac - 1);
 	if (ac == 1 || (ac == 2 && !av[1][0]))
 		return (1);
 	else if (ac == 2)
 		av = ft_split(av[1], ' ');
-	init_stack_a(a, av + 1);
-	if (!stack_sorted(*a))
+	init_stack_a(&a, av + 1);
+	if (!stack_sorted(a))
 	{
-		if (ft_stack_size(*a) == 2)
-			sa_sb(a, 0);
-		else if (ft_stack_size(*a) == 3)
-			if_three(a);
+		if (ft_stack_size(a) == 2)
+			sa_sb(&a, 0);
+		else if (ft_stack_size(a) == 3)
+			if_three(&a);
 		else
-			turk(a, b);
-		return (0);
+			turk(&a, &b);
 	}
+	free_sa_la(&a);
+	return (0);
 }
